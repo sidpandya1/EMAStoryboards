e182be69d401d022208bd73374e27ee03f924de0
Garrett <gwimer84@gmail.com>
2023-03-14 10:55:34 -0400

    fucking changes

--- EMASeniorDesignStoryBoard/Views/FirstScreen.swift
+++ EMASeniorDesignStoryBoard/Views/FirstScreen.swift
@@ -4,9 +4,7 @@
 //
 //  Created by Siddharth on 2/12/23.
 //
-
 import UIKit
-
 let passwordField: UITextField = UITextField(frame: CGRect(x: 50, y: 325, width: 300.00, height: 30.00));
 let userIDField: UITextField = UITextField(frame: CGRect(x: 50, y: 250, width: 300.00, height: 30.00));
 let loginButton = UIButton(frame:CGRect(x:125,y:400,width:150, height: 50))
@@ -14,54 +12,55 @@ let login = LoginFunctions();
 let EMAlogo = "EMALogo 1-2.png"
 let image = UIImage(named: EMAlogo)
 let emalogo = UIImageView(image: image!)
-
 let PSUlogo = "penn_state_behrend_logo_3-4in (1)-1 1"
 let image2 = UIImage(named: PSUlogo)
 let psulogo = UIImageView(image: image2!)
 let serverCon = ServerConnect();
-
 class FirstScreen: UIViewController {
     var serverconnection = ServerConnect();
     override func viewDidLoad() {
-		super.viewDidLoad()
-		serverCon.betterrecivesurvey()
-		self.view.backgroundColor = .white
-		emalogo.frame = CGRect(x: 80, y: 45, width: 250, height:200)
-		emalogo.center.x = self.view.center.x
-		view.addSubview(emalogo)
-		psulogo.frame = CGRect(x: 80, y: 500, width: 250, height:100)
-		psulogo.center.x = self.view.center.x
-		view.addSubview(psulogo)
-		userIDField.placeholder = "User ID"
-		userIDField.borderStyle = UITextField.BorderStyle.line
-		userIDField.backgroundColor = UIColor.white
-		userIDField.center.x = self.view.center.x
-		self.view.addSubview(userIDField)
-		passwordField.placeholder = "Password"
-		passwordField.isSecureTextEntry = true
-		passwordField.borderStyle = UITextField.BorderStyle.line
-		passwordField.backgroundColor = UIColor.white
-		passwordField.center.x = self.view.center.x
-		self.view.addSubview(passwordField)
-		setupButton()
-	}
-		  
-		  func setupButton() {
-			  view.addSubview(loginButton)
-			  loginButton.configuration = .filled()
-			  loginButton.configuration?.baseBackgroundColor = .systemBlue
-			  loginButton.configuration?.title = "Login"
-			  loginButton.center.x = self.view.center.x
-			  loginButton.addTarget(self, action: #selector(loginFunction), for: .touchUpInside)
-			
-		  }
-		  
-		  @objc func loginFunction() {
-			  let userID = userIDField.text!
-			  let password = passwordField.text!
-			  if(login.ValidateUsername(username: String(userID)) == true && login.ValidatePassword(password: password) == true){
-				  navigationController?.pushViewController(SurveyManager.Survey.firstQuestion(), animated: true)
-			  }
-		  }
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .white
+        serverCon.Recieve_Survey()
+        emalogo.frame = CGRect(x: 80, y: 45, width: 250, height:200)
+        emalogo.center.x = self.view.center.x
+        view.addSubview(emalogo)
+        psulogo.frame = CGRect(x: 80, y: 500, width: 250, height:100)
+        psulogo.center.x = self.view.center.x
+        view.addSubview(psulogo)
+        userIDField.placeholder = "User ID"
+        userIDField.borderStyle = UITextField.BorderStyle.line
+        userIDField.backgroundColor = UIColor.white
+        userIDField.center.x = self.view.center.x
+        self.view.addSubview(userIDField)
+        passwordField.placeholder = "Password"
+        passwordField.isSecureTextEntry = true
+        passwordField.borderStyle = UITextField.BorderStyle.line
+        passwordField.backgroundColor = UIColor.white
+        passwordField.center.x = self.view.center.x
+        self.view.addSubview(passwordField)
+        setupButton()
+    }
+          
+          func setupButton() {
+              view.addSubview(loginButton)
+              loginButton.configuration = .filled()
+              loginButton.configuration?.baseBackgroundColor = .systemBlue
+              loginButton.configuration?.title = "Login"
+              loginButton.center.x = self.view.center.x
+              loginButton.addTarget(self, action: #selector(loginFunction), for: .touchUpInside)
+            
+          }
+          
+    @objc func loginFunction() {
+        let userID = userIDField.text!
+        let password = passwordField.text!
+        if(serverCon.Sign_in(password: password, username: userID) == true){
+            
+            navigationController?.pushViewController(SurveyManager.Survey.firstQuestion(), animated: true)
+            
+        }
+    }
+      }
 
-	  }
--- EMASeniorDesignStoryBoard/functions/ServerConnect.swift
+++ EMASeniorDesignStoryBoard/functions/ServerConnect.swift
@@ -4,25 +4,46 @@
 //
 //  Created by gwimer on 2/13/23.
 //
-
 import Foundation
-
 class ServerConnect{
     let Port_number = 0
+    var userID = 0;
+    var deviceID = 100;
     
     let Password = ""
     let Username = ""
     
     func Sign_in(password:String,username:String) ->Bool {
-        if(username == Username &&  password == Password){
-            return true
-        }
-        else{
-            return false
-        }
+        userID = Int(username) ?? 0
         
+        
+        let url = URL(string: "https://psubehrendema.org/checkUser.php")
+               guard let requestUrl = url else { fatalError() }
+               var request = URLRequest(url: requestUrl)
+               request.httpMethod = "POST"
+               // Set HTTP Request Header
+               request.setValue("application/json", forHTTPHeaderField: "Accept")
+               request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+               
+               let encoder = JSONEncoder()
+               var mypost = http_post(userID: userID,deviceID: deviceID)
+               
+               
+               let jsondata = try! encoder.encode(mypost)
+               print(jsondata)
+               request.httpBody = jsondata
+               let task: Void = URLSession.shared.dataTask(with: request) { data, response, error in
+                   if let data = data {
+                       print(String(decoding: data, as: UTF8.self))
+                   } else if let error = error {
+                       print("HTTP Request Failed \(error)")
+                   }
+                   print("number of questions:", SurveyArray.allQuestions.count)
+               }
+                   .resume()
+        return true;
     }
-    func betterrecivesurvey(){
+    func Recieve_Survey(){
         let url = URL(string: "https://psubehrendema.org/getSurvey.php")
                guard let requestUrl = url else { fatalError() }
                var request = URLRequest(url: requestUrl)
@@ -42,7 +63,7 @@ class ServerConnect{
                    if let data = data {
                        if let Q = try? JSONDecoder().decode([Question].self, from: data) {
                            SurveyArray.allQuestions = Q
-						   print(SurveyArray.allQuestions)
+                           print(SurveyArray.allQuestions)
                        } else {
                            print("Error!")
                        }
@@ -55,7 +76,7 @@ class ServerConnect{
         
         
     }
-	
+    
 }
     
     
