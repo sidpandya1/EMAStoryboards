d48e909ff3b401996d1cdaa7cffe0739cdf42898
Ryan Pape <ryan.pape5@gmail.com>
2023-03-25 16:12:56 -0400

    Added Views

---
+++ EMASeniorDesignStoryBoard/Views/OpenEndedView.swift
@@ -0,0 +1,75 @@
+//
+//  OpenEndedView.swift
+//  EMASeniorDesignStoryBoard
+//
+//  Created by Ryan Pape on 3/24/23.
+//
+
+import UIKit
+private let question = UILabel(frame:CGRect(x: 37, y: 100, width: 300.00, height: 300.00))
+private let previousButton = UIButton(frame:CGRect(x:25,y:600,width:150, height: 50))
+private let nextButton = UIButton(frame:CGRect(x:200,y:600,width:150, height: 50))
+private let answerField = UITextField(frame: CGRect(x: 0, y: 250, width: 300.00, height:100.00));
+class OpenEndedView: UIViewController{
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		self.view.backgroundColor = .white
+		showQuestion()
+		showAnswerBox()
+		previousQuestionButton()
+		nextQuestionButton()
+		navigationItem.hidesBackButton = true
+		
+	}
+	
+	func showAnswerBox(){
+		answerField.placeholder = "Enter your response here"
+		answerField.borderStyle = UITextField.BorderStyle.line
+		answerField.backgroundColor = UIColor.white
+		answerField.center.x = self.view.center.x
+		answerField.center.y = self.view.center.y + 50
+		view.addSubview(answerField)
+
+	}
+	
+	func showQuestion() {
+		question.text = SurveyManager.Survey.getCurrentQuestion()
+		print(SurveyManager.Survey.getQuestionID())
+		question.backgroundColor = .systemBlue
+		question.textColor = UIColor.white
+		question.textAlignment = .center
+		question.numberOfLines = 0
+		question.center.x = self.view.center.x
+		self.view.addSubview(question)
+	}
+ 
+	
+	func nextQuestionButton() {
+		view.addSubview(nextButton)
+		nextButton.configuration = .filled()
+		nextButton.configuration?.baseBackgroundColor = .systemBlue
+		nextButton.configuration?.title = "Next"
+		nextButton.center.x = self.view.center.x + 100
+		nextButton.addTarget(self, action: #selector(goToNextQuestion), for: .touchUpInside)
+	}
+	
+	func previousQuestionButton() {
+		view.addSubview(previousButton)
+		previousButton.configuration = .filled()
+		previousButton.configuration?.baseBackgroundColor = .systemBlue
+		previousButton.configuration?.title = "Previous"
+		previousButton.center.x = self.view.center.x - 100
+		previousButton.addTarget(self, action: #selector(goToPreviousQuestion), for: .touchUpInside)
+
+	}
+	@objc private func goToNextQuestion() {
+		JSONEncoding.encoderJSON.addAnswerToArray(id: String(SurveyManager.Survey.getQuestionID()), answer: answerField.text!)
+		answerField.text?.removeAll()
+		navigationController?.pushViewController(SurveyManager.Survey.nextQuestion(), animated: true)
+	}
+	
+	@objc private func goToPreviousQuestion() {
+		navigationController?.pushViewController(SurveyManager.Survey.previousQuestion(), animated: true)
+	}
+	
+}
